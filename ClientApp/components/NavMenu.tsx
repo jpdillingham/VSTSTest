b4e0c2defef093cd203b74af2b96e7d9e9aad67a
JP Dillingham <jpdillingham@gmail.com>
2018-07-11 18:11:48 -0500

    bump

--- ClientApp/components/NavMenu.tsx
+++ ClientApp/components/NavMenu.tsx
@@ -12,7 +12,7 @@ export class NavMenu extends React.Component<{}, {}> {
                         <span className='icon-bar'></span>
                         <span className='icon-bar'></span>
                     </button>
-                    <Link className='navbar-brand' to={ '/' }>App</Link>
+                    <Link className='navbar-brand' to={ '/' }>VSTSTest</Link>
                 </div>
                 <div className='clearfix'></div>
                 <div className='navbar-collapse collapse'>
